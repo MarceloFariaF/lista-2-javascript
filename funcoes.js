62ecc118bb955617b7d84c8231aae2f7d87e7603
Marcelo Faria <vanusagallate@gmail.com>
2017-05-25 00:37:12 -0300

    Terminando JavaScript Erro?????

--- funcoes.js
+++ funcoes.js
@@ -190,5 +190,19 @@ function calcula14(){
     document.getElementById("mensagem").innerHTML = "R$" + mensagem
 }
 function calcula15() {
-    
+    var valor = document.getElementById("valor").value;
+    var esc = document.getElementsByName("esc").value;
+    var renda
+
+    if (esc = "invest") {
+        renda = (renda * 0.03);
+        arredondado = parseFloat(renda.toFixed(2));
+        mensagem = arredondado
+    }
+    else if (esc = "invest2") {
+        renda = (renda * 0.04);
+        arredondado = parseFloat(renda.toFixed(2));
+        mensagem = arredondado
+    }
+    document.getElementById("mensagem").innerHTML = mensagem
 }
