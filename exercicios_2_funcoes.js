9ee6d72e32f5388b8f8e844906ba09e68a5ff8eb
Marcelo Faria <vanusagallate@gmail.com>
2017-06-08 00:26:08 -0300

    Exercicio 21,22,23,25

--- exercicios_2_funcoes.js
+++ exercicios_2_funcoes.js
@@ -91,7 +91,7 @@ function calcula21() {
             mensagem ="O teu produto tem procedência 'Nordeste' e o valor de R$" + preco;
             break;
         case 30:
-            mensagem ="O teu produto tem procedência 'Nordeste' e o valor de R$" + preco;
+            mensagem =("O teu produto tem procedência 'Nordeste' e o valor de R$" + preco);
             break;
     }
 
@@ -137,4 +137,101 @@ function calcula22() {
         }
     }
     document.getElementById("mensagem2").innerHTML = mensagem2;
+}
+function calcula23() {
+    var codi = document.getElementById("codi").value;
+    var quanti = document.getElementById("quanti").value;
+    var ptotal, pdesconto;
+
+
+    if ((codi >= 1) && (codi <=10)) {
+        ptotal = 10 * quanti;
+        if (ptotal <= 250) {
+            pdesconto = ptotal - (ptotal * 0.05);
+            mensagem3 = ("O Valor Unitário do produto comprado é R$10,00 </br>" + "O Preço total da nota é de R$" + ptotal + "</br>" + "Você tera um desconto de 5% </br>" + "O preço com desconto será: R$" + pdesconto);
+        }
+        else if ((ptotal > 250) && (ptotal <= 500)) {
+            pdesconto = ptotal - (ptotal * 0.1);
+            mensagem3 = ("O Valor Unitário do produto comprado é R$10,00 </br>" + "O Preço total da nota é de R$" + ptotal + "</br>" + "Você tera um desconto de 10% </br>" + "O preço com desconto será: R$" + pdesconto);
+        }
+        else {
+            pdesconto = ptotal - (ptotal * 0.15);
+            mensagem3 = ("O Valor Unitário do produto comprado é R$10,00 </br>" + "O Preço total da nota é de R$" + ptotal + "</br>" + "Você tera um desconto de 15% </br>" + "O preço com desconto será: R$" + pdesconto);
+        }
+    }
+    else if ((codi > 10) && (codi <= 20)) {
+        ptotal = 15 * quanti;
+        if (ptotal <= 250) {
+            pdesconto = ptotal - (ptotal * 0.05);
+            mensagem3 = ("O Valor Unitário do produto comprado é R$15,00 </br>" + "O Preço total da nota é de R$" + ptotal + "</br>" + "Você tera um desconto de 5% </br>" + "O preço com desconto será: R$" + pdesconto);
+        }
+        else if ((ptotal > 250) && (ptotal <= 500)) {
+            pdesconto = ptotal - (ptotal * 0.1);
+            mensagem3 = ("O Valor Unitário do produto comprado é R$15,00 </br>" + "O Preço total da nota é de R$" + ptotal + "</br>" + "Você tera um desconto de 10% </br>" + "O preço com desconto será: R$" + pdesconto);
+        }
+        else {
+            pdesconto = ptotal - (ptotal * 0.15);
+            mensagem3 = ("O Valor Unitário do produto comprado é R$15,00 </br>" + "O Preço total da nota é de R$" + ptotal + "</br>" + "Você tera um desconto de 15% </br>" + "O preço com desconto será: R$" + pdesconto);
+        }
+    }
+    else if ((codi > 20) && (codi <= 30)) {
+        ptotal = 20 * quanti;
+        if (ptotal <= 250) {
+            pdesconto = ptotal - (ptotal * 0.05);
+            mensagem3 = ("O Valor Unitário do produto comprado é R$20,00 </br>" + "O Preço total da nota é de R$" + ptotal + "</br>" + "Você tera um desconto de 5% </br>" + "O preço com desconto será: R$" + pdesconto);
+        }
+        else if ((ptotal > 250) && (ptotal <= 500)) {
+            pdesconto = ptotal - (ptotal * 0.1);
+            mensagem3 = ("O Valor Unitário do produto comprado é R$20,00 </br>" + "O Preço total da nota é de R$" + ptotal + "</br>" + "Você tera um desconto de 10% </br>" + "O preço com desconto será: R$" + pdesconto);
+        }
+        else {
+            pdesconto = ptotal - (ptotal * 0.15);
+            mensagem3 = ("O Valor Unitário do produto comprado é R$20,00 </br>" + "O Preço total da nota é de R$" + ptotal + "</br>" + "Você tera um desconto de 15% </br>" + "O preço com desconto será: R$" + pdesconto);
+        }
+    }
+    else if ((codi > 30) && (codi <= 40)) {
+        ptotal = 30 * quanti;
+        if (ptotal <= 250) {
+            pdesconto = ptotal - (ptotal * 0.05);
+            mensagem3 = ("O Valor Unitário do produto comprado é R$30,00 </br>" + "O Preço total da nota é de R$" + ptotal + "</br>" + "Você tera um desconto de 5% </br>" + "O preço com desconto será: R$" + pdesconto);
+        }
+        else if ((ptotal > 250) && (ptotal <= 500)) {
+            pdesconto = ptotal - (ptotal * 0.1);
+            mensagem3 = ("O Valor Unitário do produto comprado é R$30,00 </br>" + "O Preço total da nota é de R$" + ptotal + "</br>" + "Você tera um desconto de 10% </br>" + "O preço com desconto será: R$" + pdesconto);
+        }
+        else {
+            pdesconto = ptotal - (ptotal * 0.15);
+            mensagem3 = ("O Valor Unitário do produto comprado é R$30,00 </br>" + "O Preço total da nota é de R$" + ptotal + "</br>" + "Você tera um desconto de 15% </br>" + "O preço com desconto será: R$" + pdesconto);
+        }
+    }
+    else {
+        mensagem3 = ("Código Invalido");
+    }
+    document.getElementById("mensagem3").innerHTML = mensagem3;
+}
+function calcula25() {
+    var htrab = document.getElementById("htrab").value;
+    var hfalt = document.getElementById("hfalt").value;
+    var h = htrab - (2/3 * (hfalt));
+    var min = h * 60;
+
+    if (min > 2400) {
+        mensagem4 = ("Seu prêmio será R$500,00");
+    }
+    else if ((min > 1800) && (min <= 2400)) {
+        mensagem4 = ("Seu prêmio será R$400,00");
+    }
+    else if ((min > 1200) && (min <= 1800)) {
+        mensagem4 = ("Seu prêmio será R$300,00");
+    }
+    else if ((min >600) && (min <= 1200)) {
+        mensagem4 = ("Seu prêmio será R$200,00");
+    }
+    else if (min < 600) {
+        mensagem4 = ("Seu prêmio será R$100,00");
+    }
+    else {
+        mensagem4 = ("Dados Invalidos");
+    }
+    document.getElementById("mensagem4").innerHTML = mensagem4;
 }
